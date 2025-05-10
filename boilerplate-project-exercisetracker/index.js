8341c54cdd31a4666f2a20a26041ed5d3e0f1723
Rogers-Wandera <rogerrisha@gmail.com>
2025-05-10 10:07:17 +0300

    finalized the exercise tracker project

--- boilerplate-project-exercisetracker/index.js
+++ boilerplate-project-exercisetracker/index.js
@@ -29,7 +29,13 @@ const User = mongoose.model("User", userSchema);
 
 app.use(cors());
 app.use(express.static("public"));
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use((req, res, next) => {
+  console.log(`${req.method} ${req.path} - ${req.ip}`);
+  next();
+});
 
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/views/index.html");
@@ -68,7 +74,7 @@ app.post("/api/users", async (req, res) => {
 
 app.post("/api/users/:_id/exercises", async (req, res) => {
   let { description, duration, date } = req.body;
-  const _id = req.body[":_id"];
+  const _id = req.params._id;
   try {
     const userExists = await User.findById(_id);
     if (!userExists) {
@@ -81,15 +87,17 @@ app.post("/api/users/:_id/exercises", async (req, res) => {
     userExists.log.push(exercise);
     userExists.count += 1;
     await userExists.save();
-
-    res.json({
+    const json = {
       _id,
       username: userExists.username,
       description,
       duration: parseInt(duration),
       date: new Date(date).toDateString(),
-    });
+    };
+    console.log(json);
+    res.json(json);
   } catch (err) {
+    console.log(err.message);
     res.status(400).json({ error: err.message });
   }
 });
